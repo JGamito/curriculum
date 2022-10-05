7c853629b01f76fb3d9fed5550fe65493a831dfd
JGamito <gamito90@hotmail.com>
2022-10-05 23:40:18 +0200

    Add Projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import face1 from "./face1.png";
 import styles from "./App.module.css";
 import NavBar from "./components/navBar/navBar";
-
+import ricon from "./ricon.png";
+import pcmarkt from "./pcmarkt.png";
+import pcmarktmobile from "./pcmarktmobile.png";
 function App() {
   return (
     <div className={styles.all}>
       <header>
         <NavBar />
+      </header>
+      <body>
         <div className={styles.divPhoto}>
           <img className={styles.photo} alt="My Face" src={face1} />
           <h6 className={styles.h6}>
@@ -21,9 +25,30 @@ function App() {
             I love learn new technologies and develop my logic to find
             solutions.
           </h6>
+          <br />
+          <br />
+          <div className={styles.projects}>
+            <h6 className={styles.h6}>
+              My first big Project! Created using React!
+            </h6>
+            <img className={styles.react} alt="react Logo" src={ricon} />
+          </div>
+          <div className={styles.projectsphoto}>
+            <img
+              className={styles.pcmarkt}
+              alt="image web demo"
+              src={pcmarkt}
+            />
+            <img
+              className={styles.pcmarktmobile}
+              alt="image web demo2"
+              src={pcmarktmobile}
+            />
+          </div>
         </div>
-      </header>
-      <body></body>
+      </body>
+
+      <div></div>
     </div>
   );
 }
