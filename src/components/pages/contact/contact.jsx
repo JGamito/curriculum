d283dfc7a96f5be44c2d2b11f88864207b74ba94
JGamito <gamito90@hotmail.com>
2022-10-20 15:16:28 +0200

    demo2

--- src/components/pages/contact/contact.jsx
+++ src/components/pages/contact/contact.jsx
@@ -4,7 +4,7 @@ import styles from "./contact.module.css";
 const Contact = () => {
   return (
     <div>
-      <div className={styles.all}>
+      <div className={styles.contact}>
         <h1>contact me </h1>
       </div>
     </div>
