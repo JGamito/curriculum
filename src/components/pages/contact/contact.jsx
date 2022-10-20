9ef7de4b15082cda865188bc13568a3ded462636
JGamito <gamito90@hotmail.com>
2022-10-20 16:49:53 +0200

    new elements added

--- src/components/pages/contact/contact.jsx
+++ src/components/pages/contact/contact.jsx
@@ -3,9 +3,14 @@ import styles from "./contact.module.css";
 
 const Contact = () => {
   return (
-    <div>
-      <div className={styles.contact}>
-        <h1>contact me </h1>
+    <div className={styles.full}>
+      <div className={styles.card}>
+        <h4 className={styles.h4}> Contact me in my social media:</h4>
+        <a href="https://www.linkedin.com/in/jgamitomartin/">Linkedin</a>
+        <br />
+        <a href="https://github.com/JGamito">GitHub</a>
+        <br />
+        <a href="mailto:gamito90@hotmail.com">Correo electronico</a>
       </div>
     </div>
   );
